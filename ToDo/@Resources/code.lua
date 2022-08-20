9b094b55a1e1fa4413166bd755a5bee5ccb3879f
MASTREX <38161604+MASTREX@users.noreply.github.com>
2022-08-20 22:17:31 +0530

    Initial Commit

---
+++ ToDo/@Resources/code.lua
@@ -0,0 +1,24 @@
+function ReadFile()
+	FilePath = SELF:GetOption('FilePath', '')
+	-- HANDLE RELATIVE PATH OPTIONS.
+	FilePath = SKIN:MakePathAbsolute(FilePath)
+
+	-- OPEN FILE.
+	local File = io.open(FilePath)
+
+	-- HANDLE ERROR OPENING FILE.
+	if not File then
+		print('ReadFile: unable to open file at ' .. FilePath)
+		return
+	end
+
+	-- READ FILE CONTENTS AND CLOSE.
+	local Contents = File:read('*all')
+	File:close()
+
+	return Contents
+end
+
+function Update()
+	return ReadFile()
+end
